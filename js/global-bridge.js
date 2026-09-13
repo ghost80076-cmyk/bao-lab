b7a2285bc17531bbdee06ec6233c4f85fc93a063
ghost80076-cmyk <ghost80076@gmail.com>
2026-09-13 23:38:50 +0800

    Expose app globals for optional runtime modules

---
+++ js/global-bridge.js
@@ -0,0 +1,5 @@
+(() => {
+  if (typeof App !== "undefined") window.App = App;
+  if (typeof Chat !== "undefined") window.Chat = Chat;
+  if (typeof GameState !== "undefined") window.GameState = GameState;
+})();
