04673002c01a4059f5ff5e311d19490b51c4e989
ghost80076-cmyk <ghost80076@gmail.com>
2026-09-14 01:23:46 +0800

    Load on-demand world context injector

--- js/site-ui.js
+++ js/site-ui.js
@@ -13,6 +13,7 @@ window.addEventListener("DOMContentLoaded", () => {
     .then(() => loadBAOScript("js/world-modules.js"))
     .then(() => loadBAOScript("js/world-state-cost.js"))
     .then(() => loadBAOScript("js/world-module-ui.js"))
+    .then(() => loadBAOScript("js/world-relevance.js"))
     .then(() => loadBAOScript("js/world-state-hook.js"))
     .catch(err => console.warn("BAO/LAB world state modules failed to load:", err));
   loadBAOScript("js/cost-control.js")
