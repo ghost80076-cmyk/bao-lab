91b7395f4c5fba218682e630aeb6828b79a7264f
ghost80076-cmyk <ghost80076@gmail.com>
2026-09-15 21:53:08 +0800

    test: add Firefox and WebKit E2E matrix

--- playwright.config.cjs
+++ playwright.config.cjs
@@ -9,10 +9,14 @@ module.exports = defineConfig({
   reporter: process.env.CI ? [["github"], ["html", { open: "never" }]] : "list",
   use: {
     baseURL: "http://127.0.0.1:4173",
-    browserName: "chromium",
     screenshot: "only-on-failure",
     trace: "retain-on-failure"
   },
+  projects: [
+    { name: "chromium", use: { browserName: "chromium" } },
+    { name: "firefox", use: { browserName: "firefox" } },
+    { name: "webkit", use: { browserName: "webkit" } }
+  ],
   webServer: {
     command: "python3 -m http.server 4173 --bind 127.0.0.1",
     url: "http://127.0.0.1:4173",
